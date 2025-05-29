03282dc436ba35d4c969500985822fba9eaaf0ad
vnd <vndxoxo@gmail.com>
2025-05-30 02:54:50 +0530

    eslint ignore

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['dbuzz-assets.s3.amazonaws.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
